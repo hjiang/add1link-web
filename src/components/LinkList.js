257cdc8988d69d20040653e7b9d97989ddc311db
Hong Jiang <hjiang@leancloud.rocks>
2018-06-03 13:18:02 +0800

    Actually fetch the links.

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 import Link from './Link';
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        title: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        title: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ];
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>;
+    }
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>{this.props.feedQuery.error}</div>;
+    }
 
+    const linksToRender = this.props.feedQuery.feed.links;
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
     );
   }
 }
 
-export default LinkList;
+LinkList.propTypes = {
+  feedQuery: PropTypes.object.isRequired
+};
+
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        url
+        title
+      }
+      nextTimestamp
+    }
+  }
+`;
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' }) (LinkList);
