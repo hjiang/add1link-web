1fd1f02f91d5b56ae552d54c0178200f0ab46293
Hong Jiang <j@1byte.io>
2021-02-02 23:05:02 +0800

    refactor: start moving to create-react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect';
 import { ApolloClient } from 'apollo-client';
@@ -23,14 +21,13 @@ const userIsAuthenticated = connectedRouterRedirect({
   redirectPath: '/login',
   // If selector is true, wrapper will not redirect
   // For example let's check that state contains user data
-  authenticatedSelector: state => state.auth.user !== null &&
-    state.auth.token !== null,
+  authenticatedSelector: (state) =>
+    state.auth.user !== null && state.auth.token !== null,
   // A nice display name for this check
   wrapperDisplayName: 'UserIsAuthenticated'
 });
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     const httpLink = new HttpLink({ uri: 'https://add1.link/graphql/' });
@@ -51,39 +48,53 @@ class App extends React.Component {
     });
   }
 
-
   render() {
     return (
       <ApolloProvider client={this.apolloClient}>
-        <Container style={{ width: '100%', margin: 0 }}>
-          <HeaderNav />
-          <Container style={{ width: '100%', margin: 0, minHeight: '20em' }}>
-            <Switch>
-              <Route exact path='/' component={LandingPage} />
-              <Route path="/links" component={userIsAuthenticated(LinksPage)} />
-              <Route path="/sign-up" component={SignUpPage} />
-              <Route path="/login" component={LoginPage} />
-              <Route component={NotFoundPage} />
-            </Switch>
+        <Router>
+          <Container style={{ width: '100%', margin: 0 }}>
+            <HeaderNav />
+            <Container style={{ width: '100%', margin: 0, minHeight: '20em' }}>
+              <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route
+                  path="/links"
+                  component={userIsAuthenticated(LinksPage)}
+                />
+                <Route path="/sign-up" component={SignUpPage} />
+                <Route path="/login" component={LoginPage} />
+                <Route component={NotFoundPage} />
+              </Switch>
+            </Container>
+            <Container text style={{ marginTop: '5em', textAlign: 'center' }}>
+              <p>
+                Copied with ❤️ from StackOverflow. Hosted on{' '}
+                <a href="https://leancloud.cn">LeanCloud</a> (US West).
+              </p>
+              <p>
+                Browser extensions:{' '}
+                <a
+                  href="https://chrome.google.com/webstore/detail/add-1-link-extension/npjhpkhnhdkcglekeailemkebeceigka"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Chrome
+                </a>{' '}
+                |{' '}
+                <a
+                  href="https://addons.mozilla.org/en-US/firefox/addon/1-link-extension/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Firefox
+                </a>
+              </p>
+            </Container>
           </Container>
-          <Container text style={{ marginTop: '5em', textAlign: 'center' }}>
-            <p>Copied with ❤️ from StackOverflow.
-            Hosted on <a href="https://leancloud.cn">LeanCloud</a> (US West).</p>
-            <p>Browser extensions: <a href="https://chrome.google.com/webstore/detail/add-1-link-extension/npjhpkhnhdkcglekeailemkebeceigka" target="_blank" rel="noopener noreferrer">Chrome</a> | <a href="https://addons.mozilla.org/en-US/firefox/addon/1-link-extension/" target="_blank" rel="noopener noreferrer">Firefox</a></p>
-          </Container>
-        </Container>
+        </Router>
       </ApolloProvider>
     );
   }
 }
 
-App.propTypes = {
-  children: PropTypes.element,
-  auth: PropTypes.object,
-};
-
-function mapStateToProps(state) {
-  return { auth: state.auth };
-}
-
-export default withRouter(connect(mapStateToProps)(App));
+export default App;
